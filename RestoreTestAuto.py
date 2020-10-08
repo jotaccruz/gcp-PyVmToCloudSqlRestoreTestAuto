35d7fbe8312a44d2c9297692fa9fa2fe695488b8
Juan Carlos Cruz Salazar <juan.cruz2@telusinternational.com>
2020-10-08 01:32:17 -0600

    The whole Python project

--- RestoreTestAuto.py
+++ RestoreTestAuto.py
@@ -9,8 +9,8 @@ import time
 
 from six.moves import input
 
-from google.cloud import storage
-from googleapiclient.discovery import build
+from google.cloud import storage  #cloud storage library
+from googleapiclient.discovery import build #cloud api client library
 from google.oauth2 import service_account
 import json
 
@@ -43,11 +43,8 @@ def list_sql_instances(cloudsql,projectname,sqlinstance_name):
 			if items['name'].startswith(sqlinstance_name):
 				sqlinstances['name'] = items['name']
 				sqlinstances['email'] = items['serviceAccountEmailAddress']
-				EmailAddress=items['serviceAccountEmailAddress']
 				if items['ipAddresses'][0]['type']=='PRIMARY':
 					sqlinstances['ip'] = [items['ipAddresses'][0]['ipAddress']]
-	#https://cloud.google.com/sql/docs/sqlserver/import-export/importing
-	add_bucket_iam_member("dba-freenas","roles/storage.admin","serviceAccount:" + EmailAddress)
 	return sqlinstances
 # [END list_sql_instances]
 
@@ -132,10 +129,10 @@ def create_sqlinstance(cloudsql, project, zone, sqlinstance_name, machine_type, 
 
 
 # [START import_instance]
-def import_sqlinstance(cloudsql, project, sqlinstance_name,database_name,filetype):
+def import_sqlinstance(cloudsql, project, sqlinstance_name, uri, database_name, filetype):
     config = {
         'importContext': {
-            'uri': 'gs://dba-freenas/SUSWEYAK15_EvoDb_Testing_FULL_20200325_011850.bak',
+            'uri': uri,
             'database': database_name,
             'fileType': filetype
         }
@@ -146,84 +143,88 @@ def import_sqlinstance(cloudsql, project, sqlinstance_name,database_name,filetyp
     	instance=sqlinstance_name,
     	body=config).execute()
 # [END import_instance]
-# import_sqlinstance(cloudsql,"ti-is-devenv-01","us-west1-a",instances['targetID'],'EvoDb_Testing','BAK')
+# import_sqlinstance(cloudsql,"ti-is-devenv-01","us-west1-a",instances['name'],uri,'EvoDb_Testing','BAK')
 
-
-# [START run]
-def main(project, bucket, zone, sqlinstance_name, wait=True):
-    # Construct the service object for the interacting with the Cloud SQL Admin API.
-    cloudsql = build('sqladmin','v1beta4')
-
-    print('Creating the CloudSQL Instance...')
-
-    operation = create_sqlinstance(cloudsql,"ti-is-devenv-01","us-west1-a",generate_random_name(sqlinstance_name,5),"db-custom-4-15360",100,'SQLSERVER_2017_WEB',"Pass12345")
-    wait_for_operation(cloudsql, project, operation['name'])
-
-    print("""
-CloudSQL Instance created.
-It will take a minute or two for the instance to complete work.
-Once the CloudSQL Instance is created press enter to IMPORT a database.
-""".format(project))
-
-    instances = list_sql_instances(cloudsql, project,operation['targetId'])
-
-    print('CloudSQL Instances in project %s and zone %s:' % (project, zone))
-    print(instances['name'])
-    print(instances['email'])
-    print(instances['ip'])
-
-    if wait:
-        input()
-
-    print('Importing a Database.')
-
-    operation = import_sqlinstance(cloudsql, project, instances['name'],'EvoDb_Testing','BAK')
-    wait_for_operation(cloudsql, project, operation['name'])
-
-
-    print("""
-Database imported.
-Once the Database is imported press enter to DELETE the instance.
-""".format(project))
-
-    if wait:
-        input()
-
-    print('Deleting CloudSQL instance.')
-
-    operation = destroy_sqlinstance(cloudsql, project, instances['name'])
-    wait_for_operation(cloudsql, project, operation['name'])
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('project_id', help='Your Google Cloud project ID.')
-    parser.add_argument('bucket_name', help='Your Google Cloud Storage bucket name.')
-    parser.add_argument('--zone', default='us-west1-a', help='Cloud SQL zone to deploy to.')
-    parser.add_argument('--name', default='sql1', help='New instance name.')
-
-    args = parser.parse_args()
-
-    main(args.project_id, args.bucket_name, args.zone, args.name)
-# [END run]
-#python sqlinstance.py --name sqlrestore --zone us-west1-a ti-is-devenv-01 dba-freenas
-
-#Using credentials
-#-------------------------------------------------------------------------------------------------------
-# Construct the service object for the interacting with the Cloud SQL Admin API.
-def list_sql_instances_cred(projectname,):
-	credentials = service_account.Credentials.from_service_account_file('')
-
-	scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
-
-	service = build('sqladmin', 'v1beta4',credentials=credentials)
-
-	req = service.instances().list(project=projectname)
-	resp = req.execute()
-	for key in resp:
-		for items in resp[key]:
-			if items['name']=='sql1':
-				print (items['name'])
-				print (items['serviceAccountEmailAddress'])
-				if items['ipAddresses'][0]['type']=='PRIMARY':
-					print (items['ipAddresses'][0]['ipAddress'])
+#Enable it if you want to try the script independent
+## [START run]
+#def main(project, bucket, zone, sqlinstance_name, wait=True):
+#    # Construct the service object for the interacting with the Cloud SQL Admin API.
+#    cloudsql = build('sqladmin','v1beta4')
+#
+#    print('Creating the CloudSQL Instance...')
+#
+#    operation = create_sqlinstance(cloudsql,"ti-is-devenv-01","us-west1-a",generate_random_name(sqlinstance_name,5),"db-custom-4-15360",100,'SQLSERVER_2017_WEB',"Pass12345")
+#    wait_for_operation(cloudsql, project, operation['name'])
+#
+#    print("""
+#CloudSQL Instance created.
+#It will take a minute or two for the instance to complete work.
+#Once the CloudSQL Instance is created press enter to IMPORT a database.
+#""".format(project))
+#
+#    instances = list_sql_instances(cloudsql, project,operation['targetId'])
+#
+#    print('CloudSQL Instances in project %s and zone %s:' % (project, zone))
+#    print(instances['name'])
+#    print(instances['email'])
+#    print(instances['ip'])
+#
+#https://cloud.google.com/sql/docs/sqlserver/import-export/importing
+#    add_bucket_iam_member(bucket,"roles/storage.admin","serviceAccount:" + instances['email'])
+#
+#    if wait:
+#        input()
+#
+#    print('Importing a Database.')
+#
+#    operation = import_sqlinstance(cloudsql, project, instances['name'],uri,'EvoDb_Testing','BAK')
+#    wait_for_operation(cloudsql, project, operation['name'])
+#
+#
+#    print("""
+#Database imported.
+#Once the Database is imported press enter to DELETE the instance.
+#""".format(project))
+#
+#    if wait:
+#        input()
+#
+#    print('Deleting CloudSQL instance.')
+#
+#    operation = destroy_sqlinstance(cloudsql, project, instances['name'])
+#    wait_for_operation(cloudsql, project, operation['name'])
+#
+#
+#if __name__ == '__main__':
+#    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+#    parser.add_argument('project_id', help='Your Google Cloud project ID.')
+#    parser.add_argument('bucket_name', help='Your Google Cloud Storage bucket name.')
+#    parser.add_argument('--zone', default='us-west1-a', help='Cloud SQL zone to deploy to.')
+#    parser.add_argument('--name', default='sql1', help='New instance name.')
+#
+#    args = parser.parse_args()
+#
+#    main(args.project_id, args.bucket_name, args.zone, args.name)
+## [END run]
+##python sqlinstance.py --name sqlrestore --zone us-west1-a ti-is-devenv-01 dba-freenas
+#
+##Using credentials
+##-------------------------------------------------------------------------------------------------------
+## Construct the service object for the interacting with the Cloud SQL Admin API.
+#def list_sql_instances_cred(projectname,):
+#	credentials = service_account.Credentials.from_service_account_file('')
+#
+#	scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
+#
+#	service = build('sqladmin', 'v1beta4',credentials=credentials)
+#
+#	req = service.instances().list(project=projectname)
+#	resp = req.execute()
+#	for key in resp:
+#		for items in resp[key]:
+#			if items['name']=='sql1':
+#				print (items['name'])
+#				print (items['serviceAccountEmailAddress'])
+#				if items['ipAddresses'][0]['type']=='PRIMARY':
+#					print (items['ipAddresses'][0]['ipAddress'])
+#
--- VMtoCloudRestore.py
+++ VMtoCloudRestore.py
@@ -5,7 +5,7 @@ DBM_USER = 'GenBackupUser'
 DBM_USER_PASSWORD = 'DBB@ckuPU53r*'
 DBM_NAME = 'db_legacy_maintenance'
 SERVER_NAME = 'SUSWEYAK03'
-SSL_PATH="/ssl-certs/"
+SSL_PATH='.\\ssl-certs\\'
 
 # Import required python libraries
 import time
@@ -33,7 +33,8 @@ BUCKET_GCS = 'ti-sql-02'
 BUCKET = 'ti-sql-02/Backups/Current/'
 BUCKET_PATH="/root/cloudstorage/Backups/Current/"
 
-DATETIME = time.strftime('%Y-%m-%d')
+#DATETIME = time.strftime('%Y-%m-%d')
+DATETIME = '2020-10-07'
 
 from mysql.connector.constants import ClientFlag
 from mysql.connector import Error
@@ -44,9 +45,9 @@ config = {
     'host': DBM_HOST,
     'database': DBM_NAME,
     'client_flags': [ClientFlag.SSL],
-    'ssl_ca': SSL_PATH + SERVER_NAME + '/server-ca.pem' ,
-    'ssl_cert': SSL_PATH + SERVER_NAME + '/client-cert.pem',
-    'ssl_key': SSL_PATH + SERVER_NAME + '/client-key.pem',
+    'ssl_ca': SSL_PATH + '\server-ca.pem' ,
+    'ssl_cert': SSL_PATH + '\client-cert.pem',
+    'ssl_key': SSL_PATH + '\client-key.pem',
 }
 
 #config2 = {
@@ -69,16 +70,49 @@ try:
     #cur0.close()
     #cnx0.close()
 
+    cloudsql = build('sqladmin','v1beta4')
+    sqlinstance_name = 'loveit'
+    project = "ti-is-devenv-01"
+    zone = "us-west1-a"
+    wait = True
+
+    print('Creating the CloudSQL Instance...')
+    operation = create_sqlinstance(cloudsql,project,zone,generate_random_name(sqlinstance_name,5),"db-custom-4-15360",100,'SQLSERVER_2017_WEB',"Pass12345")
+    wait_for_operation(cloudsql, project, operation['name'])
+
+    print("""
+    CloudSQL Instance created.
+    It will take a minute or two for the instance to complete work.
+    Once the CloudSQL Instance is created press enter to IMPORT a database.
+    """.format(project))
+
+    instances = list_sql_instances(cloudsql, project, operation['targetId'])
+
+    print('CloudSQL Instances in project %s and zone %s:' % (project, zone))
+    print(instances['name'])
+    print(instances['email'])
+    print(instances['ip'])
+
+    add_bucket_iam_member(BUCKET_GCS,"roles/storage.object.admin","serviceAccount:" + instances['email'])
+
+    if wait:
+        input()
+
     cnx = mysql.connector.connect(**config)
     cur = cnx.cursor()
-    cur.execute("SELECT srv_name, srv_ip, srv_user, srv_pwd, srv_directory, srv_os, srv_frecuency, srv_domain FROM lgm_servers where srv_active = 1 and srv_type = 'MYSQL'")
+    cur.execute("SELECT srv_name, srv_ip, srv_user, srv_pwd, srv_directory, srv_os, srv_frecuency, srv_domain FROM lgm_servers where srv_active = 1 and srv_type = 'MSSQL'")
     dbs = cur.fetchall()
     for rowdb in dbs:
         #print rowdb[0]
         logcmd = "select lbl_id, lbl_date, lbl_server, lbl_filename, "
-        logcmd += "SUBSTRING(lbl_filename, INSTR(lbl_filename,'_')+1, "
-        logcmd += "INSTR(lbl_filename,'.') - INSTR(lbl_filename,'_')-1) dbname "
-        logcmd += "from lgm_backups_log where lbl_filename not like '%mysql%' "
+        logcmd += "SUBSTRING(lbl_filename, INSTR(lbl_filename,'_')+1, \
+        (LENGTH(SUBSTRING(lbl_filename, INSTR(lbl_filename,'_')+1))-\
+        LENGTH(RIGHT(SUBSTRING(lbl_filename, \
+        INSTR(lbl_filename,'_')+1),25)))) dbname "
+        logcmd += "from lgm_backups_log where lbl_filename not like '%master%'\
+        and lbl_filename not like '%model%'\
+        and lbl_filename not like '%msdb%'\
+        and lbl_filename like '%FULL%' and lbl_server = 'SUSWEYAK08'"
         logcmd += " and lbl_date = '"
         logcmd += DATETIME +  "' and lbl_server = '"
         logcmd += rowdb[0]  + "' order by RAND() limit 1 "
@@ -86,40 +120,40 @@ try:
         cur2.execute(logcmd)
         dbs2 = cur2.fetchall()
         for rowdb2 in dbs2:
-            bucket = 'gs://' + BUCKET + rowdb2[2] + '/' +  rowdb2[3]
-            print (bucket)
-            #createcmd = 'mysql -h' + DB_HOST + ' -u' + DB_USER + ' -p' + DB_USER_PASSWORD + ' -e "CREATE DATABASE IF NOT EXISTS ' +  rowdb2[4] +'";'
-            #restorecmd= 'gunzip < ' + '/backup/dumps/' +  rowdb2[4] + '.sql.gz | mysql  --default-character-set=utf8 -h' + DB_HOST + ' -u' + DB_USER + ' -p' + DB_USER_PASSWORD + ' ' + rowdb2[4]
-            #delcmd = 'rm -rf ' + '/backup/dumps/' +  rowdb2[4] + '.sql.gz'
+            uri = 'gs://' + BUCKET + rowdb2[2] + '/' +  rowdb2[3]
+            print (uri)
 
-            #os.system(delcmd)
-            #os.system(bucketcmd)
-            #os.system(createcmd)
-            #os.system(restorecmd)
+            print('Importing a Database.')
+            operation = import_sqlinstance(cloudsql, project, instances['name'], uri, rowdb2[4], 'BAK')
+            wait_for_operation(cloudsql, project, operation['name'])
 
-            #icmd = "INSERT INTO db_legacy_maintenance.lgm_daily_restore(lwr_date,lwr_server,lwr_database,lwr_result) VALUES(%s,%s,%s,%s);"
-            #args = (DATETIME, rowdb2[2], rowdb2[4], 1)
+            print("""
+            Database imported.
+            Once the Database is imported press enter to DELETE the instance.
+            """.format(project))
 
-            #try:
-            #    cnx3 = mysql.connector.connect(**config)
-            #    cur3 = cnx3.cursor()
-            #    cur3.execute(icmd,args)
-            #    cnx3.commit()
-            #except Error as error:
-            #    print(error)
+            if wait:
+                input()
 
-            #finally:
-            #    cur3.close()
-            #    cnx3.close()
+            print('Deleting CloudSQL instance.')
 
-            #os.system(delcmd)
+            operation = destroy_sqlinstance(cloudsql, project, instances['name'])
+            wait_for_operation(cloudsql, project, operation['name'])
 
-            #pcmd = 'gsutil acl ch -u' + GCP_ACCOUNT + ':R gs://' + BUCKET + rowdb2[2] + '/' +  rowdb2[3]
-            #impcmd = 'gcloud sql import sql isdba-mysql-restore gs://' + BUCKET + rowdb2[2] + '/' +  rowdb2[3] + ' --quiet --database=' + rowdb2[4]
-            #os.system(pcmd)
-            #os.system(impcmd)
-            #print (pcmd)
-            #print ("Database " + rowdb2[4] + " restored")
+            icmd = "INSERT INTO db_legacy_maintenance.lgm_daily_restore(lwr_date,lwr_server,lwr_database,lwr_result) VALUES(%s,%s,%s,%s);"
+            args = (DATETIME, rowdb2[2], rowdb2[4], 1)
+
+            try:
+                cnx3 = mysql.connector.connect(**config)
+                cur3 = cnx3.cursor()
+                cur3.execute(icmd,args)
+                cnx3.commit()
+            except Error as error:
+                print(error)
+
+            finally:
+                cur3.close()
+                cnx3.close()
         cur2.close()
     cur.close()
 except mysql.connector.Error as err:
